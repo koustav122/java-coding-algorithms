086b5d4b7f68b882f01041f6e554093619d9d2a2
koustav122 <koustav1807.be22@chitkara.edu.in>
2026-02-11 10:36:16 +0530

    adding this file

--- Inner.java
+++ Inner.java
@@ -1,4 +1,15 @@
+class OuterClass {
+    int x = 10;
+    class InnerClass {
+        int y = 5;
+    }
+}
 
 public class Inner {
+    public static void main(String[] args) {
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        System.out.println(myInner.y);
+    }
     
 }
