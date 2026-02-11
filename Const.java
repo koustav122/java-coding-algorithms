f46d85f6974861aae7b501a74e0c8deecd81eaff
koustav122 <koustav1807.be22@chitkara.edu.in>
2026-02-11 10:14:06 +0530

    Initial commit

---
+++ Const.java
@@ -0,0 +1,17 @@
+class Animal{
+    Animal(){
+        System.out.println("Animal is created");
+    }
+}
+class Dog extends Animal{
+    Dog() {
+        super(); //Call Parent constructor
+        System.out.println("Dog is created");
+    }
+}
+public class Const {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+    }
+    
+}
