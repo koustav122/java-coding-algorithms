ce49f8c7b68b7329e582113dadb6722c31dae71e
koustav122 <koustav1807.be22@chitkara.edu.in>
2026-02-11 10:40:29 +0530

    adding this code

---
+++ Inner2.java
@@ -0,0 +1,13 @@
+class OuterClass{
+    int x=10;
+class InnerClass{
+    int y=5;
+}
+}
+public class Inner2{
+    public static void main(String[]args){
+        OuterClass myOuter= new OuterClass();
+        OuterClass.InnerClass myInner= myOuter.new InnerClass();
+        System.out.println(myInner.y+myOuter.x);
+    }
+}
